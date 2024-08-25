1dc57199de4af580f56e7be1c7ea45aa12df4d96
Amber Shek <mibybymi@163.com>
2024-08-25 02:50:00 +0000

    update tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,4 @@
-def my_pow(a, b):
-    print("a: {}, b: {} \nCalculating a^b...".format(a, b))
-    if a==0 and b==0:
-        # return print("Nonsense!")
-        return -1
-    c = 1
-    for i in range(b):
-        c = c * a
-    return c
+def test_passing():
+    assert (1, 2, 3) == (1, 2, 3) 
 
-print("This is a test file!")
-c = my_pow(2, 3)
-assert c == pow(2, 3)
+test_passing()
