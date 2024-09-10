86a1654017772d78eb560810ffe9de710c4b7619
Xianting Lu <38641973+xiantinglu@users.noreply.github.com>
2024-09-10 01:28:44 +0000

    change actions & files

--- fail.py
+++ fail.py
@@ -1,18 +1,13 @@
 def my_pow(a, b):
     print("a: {}, b: {} \nCalculating a^b...".format(a, b))
-    if a==0 and b==0:
-        # return print("Nonsense!")
+    if a==0:
         return -1
-    c = 1
-    for i in range(b):
-        c = c * a
-    # print("Output of a^b: {}^{} = {}".format(a, b, c))
-    return c
-
-
+    elif b==0:
+        return 1
+    else:
+        c = 1
+        for i in range(b):
+            c = c * a
+        return c
 print("This is a test file!")
-c = my_pow("2", 3)
-assert c == pow(2, 3)
-
-# c = my_pow(0, 0)
-# assert c == 0
+assert pow(1, 2)==my_pow(1, "2")
