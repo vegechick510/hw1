26797051506242ce2ea0ebf9028fb89d84830b8b
Xianting Lu <38641973+xiantinglu@users.noreply.github.com>
2024-09-10 01:28:44 +0000

    change actions & files

--- myfile.py
+++ myfile.py
@@ -1,10 +1,12 @@
 def my_pow(a, b):
     print("a: {}, b: {} \nCalculating a^b...".format(a, b))
-    if a==0 and b==0:
-        # return print("Nonsense!")
+    if a==0:
         return -1
-    c = 1
-    for i in range(b):
-        c = c * a
-    return c
-my_pow(2, "3")
+    elif b==0:
+        return 1
+    else:
+        c = 1
+        for i in range(b):
+            c = c * a
+        return 
+assert my_pow(2, 3)==my_pow(2, "3")
